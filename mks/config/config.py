5ee54535915e7ab7801cd1d30761c68778b54d02
N.A.S <88653689+konay1122@users.noreply.github.com>
2023-09-15 12:15:13 +0630

    Update config.py

--- mks/config/config.py
+++ mks/config/config.py
@@ -13,7 +13,7 @@ def is_enabled(value, default):
     
 class Config:
 
-    PORT = environ.get("PORT", "8052")
+    PORT = environ.get("PORT", "8053")
     SESSION = environ.get('SESSION', 'Media_search')
 
     API_ID = int(environ.get('API_ID', '12158462'))
